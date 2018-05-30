c6eb7ef0235ca0a5d7d94b973c3b2854c54ba5d3
Mahesh Patel <maheshpate@cybage.com>
2018-05-30 18:36:57 +0530

    change functions

--- index.js
+++ index.js
@@ -7,15 +7,17 @@ function subscription(a,b) {
     return a-b;
 }
 
-function deletion(a,b) {
-    return a/b;
-}
-
 function multiplication(a,b) {
     return a*b;
 }
 
+function deletion(a,b) {
+    return a/b;
+}
+
 module.exports = {
     addtion: addtion,
-    subscription: subscription
+    subscription: subscription,
+    multiplication:multiplication,
+    deletion:deletion
 };
